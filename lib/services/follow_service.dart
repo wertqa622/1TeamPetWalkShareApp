f850bcc03e70a992461ff77fefd971d0c6ef179b
이태경 <ltk842121@gmail.com>
2026-01-10 15:21:14 +0900

    [이태경] 팔로잉 차단 기능 수정

--- lib/services/follow_service.dart
+++ lib/services/follow_service.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import '../models/user.dart';
+import 'block_service.dart';
 
 /// 팔로우 관련 서비스 클래스
 class FollowService {
@@ -13,6 +14,12 @@ class FollowService {
   /// - users/A 문서의 following 카운트 증가
   static Future<void> followUser(String followerId, String followingId) async {
     try {
+      // 차단 관계 확인 (양방향)
+      final isBlocked = await BlockService.isBlockedBetween(followerId, followingId);
+      if (isBlocked) {
+        throw Exception('차단된 사용자는 팔로우할 수 없습니다.');
+      }
+
       // 팔로우할 유저(팔로우당한 사람)의 전체 정보 가져오기
       final followingUserDoc = await _firestore
           .collection('users')
@@ -60,18 +67,32 @@ class FollowService {
         'followedAt': FieldValue.serverTimestamp(), // 팔로우한 시점
       });
 
-      // users/{followerId}/following/{followingId} 생성 (팔로우한 유저의 팔로잉 목록)
-      // 여기에는 팔로우당한 사람(following)의 정보를 저장
+      // users/{followerId}/following/{followingNickname} 생성 (팔로우한 유저의 팔로잉 목록)
+      // 문서 ID를 nickname으로 사용 (Firestore 문서 ID에 사용할 수 없는 문자 제거/치환)
+      final followingNicknameRaw = followingUserData['nickname'] as String? ?? '';
+      if (followingNicknameRaw.isEmpty) {
+        throw Exception('팔로우할 유저의 닉네임을 찾을 수 없습니다: $followingId');
+      }
+      
+      final followingNickname = followingNicknameRaw
+          .replaceAll('/', '_')
+          .replaceAll('?', '_')
+          .replaceAll('#', '_')
+          .replaceAll('[', '_')
+          .replaceAll(']', '_')
+          .replaceAll('*', '_')
+          .trim();
+      
       final followingRef = _firestore
           .collection('users')
           .doc(followerId)
           .collection('following')
-          .doc(followingId);
+          .doc(followingNickname);
       
       batch.set(followingRef, {
         'followingId': followingId,
         'id': followingUserData['id'] ?? followingId, // 팔로우당한 사람의 ID
-        'nickname': followingUserData['nickname'] ?? '', // 팔로우당한 사람의 닉네임
+        'nickname': followingNicknameRaw, // 원본 nickname 저장
         'bio': followingUserData['bio'] ?? '', // 팔로우당한 사람의 소개
         'email': followingUserData['email'] ?? '', // 팔로우당한 사람의 이메일
         'locationPublic': followingUserData['locationPublic'] ?? true,
@@ -115,14 +136,75 @@ class FollowService {
       
       batch.delete(followersRef);
 
-      // users/{followerId}/following/{followingId} 삭제
-      final followingRef = _firestore
+      // users/{followerId}/following/{followingNickname} 또는 {followingId} 삭제
+      // 기존 데이터 호환성을 위해 nickname과 userId 둘 다 시도
+      final followingUserDoc = await _firestore
+          .collection('users')
+          .doc(followingId)
+          .get();
+      
+      if (!followingUserDoc.exists) {
+        throw Exception('언팔로우할 유저 정보를 찾을 수 없습니다: $followingId');
+      }
+      
+      final followingUserData = followingUserDoc.data()!;
+      final followingNicknameRaw = followingUserData['nickname'] as String? ?? '';
+      
+      if (followingNicknameRaw.isEmpty) {
+        throw Exception('언팔로우할 유저의 닉네임을 찾을 수 없습니다: $followingId');
+      }
+      
+      final followingNickname = followingNicknameRaw
+          .replaceAll('/', '_')
+          .replaceAll('?', '_')
+          .replaceAll('#', '_')
+          .replaceAll('[', '_')
+          .replaceAll(']', '_')
+          .replaceAll('*', '_')
+          .trim();
+      
+      // nickname으로 된 문서 삭제 시도
+      final followingRefByNickname = _firestore
           .collection('users')
           .doc(followerId)
           .collection('following')
-          .doc(followingId);
+          .doc(followingNickname);
       
-      batch.delete(followingRef);
+      final followingRefByNicknameDoc = await followingRefByNickname.get();
+      if (followingRefByNicknameDoc.exists) {
+        batch.delete(followingRefByNickname);
+        debugPrint('언팔로우: nickname 기반 문서 삭제 ($followingNickname)');
+      } else {
+        // 기존 userId 기반 문서 삭제 시도 (호환성)
+        final followingRefByUserId = _firestore
+            .collection('users')
+            .doc(followerId)
+            .collection('following')
+            .doc(followingId);
+        
+        final followingRefByUserIdDoc = await followingRefByUserId.get();
+        if (followingRefByUserIdDoc.exists) {
+          batch.delete(followingRefByUserId);
+          debugPrint('언팔로우: userId 기반 문서 삭제 ($followingId)');
+        } else {
+          // 둘 다 없으면 following 컬렉션 전체를 확인하여 followingId 필드로 찾기
+          final followingSnapshot = await _firestore
+              .collection('users')
+              .doc(followerId)
+              .collection('following')
+              .get();
+          
+          for (final doc in followingSnapshot.docs) {
+            final docData = doc.data();
+            final docFollowingId = docData['followingId'] ?? docData['id'] ?? '';
+            if (docFollowingId == followingId) {
+              batch.delete(doc.reference);
+              debugPrint('언팔로우: followingId 필드로 문서 찾아서 삭제 (${doc.id})');
+              break;
+            }
+          }
+        }
+      }
 
       // users/{followingId} 문서의 followers 카운트 감소
       final followingUserRef = _firestore.collection('users').doc(followingId);
@@ -209,8 +291,9 @@ class FollowService {
           createdAtStr = DateTime.now().toIso8601String();
         }
 
+        // 문서 ID는 nickname이므로, followingId 필드를 사용
         return User(
-          id: doc.id,
+          id: data['followingId'] ?? data['id'] ?? doc.id,
           nickname: data['nickname'] ?? '',
           bio: data['bio'] ?? '',
           email: data['email'] ?? '',
@@ -229,11 +312,37 @@ class FollowService {
   /// 특정 사용자가 다른 사용자를 팔로우하고 있는지 확인
   static Future<bool> isFollowing(String followerId, String followingId) async {
     try {
+      // followingId로 사용자 정보를 가져와서 nickname 찾기
+      final followingUserDoc = await _firestore
+          .collection('users')
+          .doc(followingId)
+          .get();
+      
+      if (!followingUserDoc.exists) {
+        return false;
+      }
+      
+      final followingUserData = followingUserDoc.data()!;
+      final followingNicknameRaw = followingUserData['nickname'] as String? ?? '';
+      
+      if (followingNicknameRaw.isEmpty) {
+        return false;
+      }
+      
+      final followingNickname = followingNicknameRaw
+          .replaceAll('/', '_')
+          .replaceAll('?', '_')
+          .replaceAll('#', '_')
+          .replaceAll('[', '_')
+          .replaceAll(']', '_')
+          .replaceAll('*', '_')
+          .trim();
+      
       final doc = await _firestore
           .collection('users')
           .doc(followerId)
           .collection('following')
-          .doc(followingId)
+          .doc(followingNickname)
           .get();
 
       return doc.exists;
@@ -268,7 +377,12 @@ class FollowService {
           .collection('following')
           .get();
 
-      return snapshot.docs.map((doc) => doc.id).toSet();
+      // 문서 ID는 nickname이므로, followingId 필드를 사용
+      return snapshot.docs
+          .map((doc) => doc.data()['followingId'] ?? doc.data()['id'] ?? doc.id)
+          .where((id) => id != null && id.isNotEmpty)
+          .cast<String>()
+          .toSet();
     } catch (e) {
       debugPrint('팔로잉 ID 목록 조회 실패: $e');
       return {};
